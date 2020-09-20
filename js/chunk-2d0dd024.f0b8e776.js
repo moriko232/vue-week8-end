5d48611cfdabea5de9f71ff741f193aeeacb84d5
LEMON\Moriko <moriko232@gmail.com>
2020-09-20 12:22:58 +0800

    增加內容

---
+++ js/chunk-2d0dd024.f0b8e776.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0dd024"],{"800f":function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an cart success page")])])}],a=n("2877"),u={},r=Object(a["a"])(u,s,c,!1,null,null,null);t["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0dd024.f0b8e776.js.map
