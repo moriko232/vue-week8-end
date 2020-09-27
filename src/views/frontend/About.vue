dee01696035e5f6871af5f15a10c43f599972c98
LEMON\Moriko <moriko232@gmail.com>
2020-09-28 00:02:13 +0800

    update about

--- src/views/frontend/About.vue
+++ src/views/frontend/About.vue
@@ -2,17 +2,31 @@
   <div class="about">
     <div class="main-content">
       <div class="container">
+        <h2>關於皂皂</h2>
         <div class="row align-items-start justify-content-center">
-          <div class="col-md-6">
-            <h2>關於皂皂</h2>
+          <div class="col-md-7">
             <img
               src="https://images.unsplash.com/photo-1591977025723-bdd717522fc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
               alt=""
             />
+          </div>
+          <div class="col-md-5">
             <div class="mt-3">
               <p>
-                用最好最天然的配方呵護您的肌膚，清爽不乾澀，無論哪種季節都能找到適合您膚質的手工皂，搭配恰到好處的極致香氛，為生活增添一點迷人優雅。
+                皂皂市集是一個專注手工肥皂的品牌
+                <br /><br />
+                我們不斷尋找，選用最天然的素材，<br />最溫和的配方， 希望帶給你美好的沐浴體驗
+                <br /><br />
+                製造好的手工皂，需要很多的堅持，維持好的品質也是，<br />我們相信這份用心，在使用時一定會有不同的感受
+
+                <br /><br />
+                用最好最天然的配方呵護您的肌膚，清爽不乾澀，<br />無論哪種季節都能找到適合您膚質的手工皂，<br />用恰到好處的極致香氛，為生活增添一點迷人優雅
               </p>
+              <p class="text-right">良心製皂－－<br>皂皂市集</p>
+              <div class="text-h3 text-right">
+                <a href="#" target="_blank"> <i class="fab fa-facebook-square"></i> </a>
+                <a href="#" target="_blank" class="ml-2"> <i class="fab fa-instagram-square"></i> </a> 
+              </div>
             </div>
           </div>
         </div>
